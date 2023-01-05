c896a92f13da2caa9d7c4ecf863addbd1131ac41
nil pinyana <nil.pinyana@gmail.com>
2023-01-05 19:37:12 +0100

    ValidacióVehicles

--- CaixaBlanca/src/VerificacioVehicles.java
+++ CaixaBlanca/src/VerificacioVehicles.java
@@ -1,67 +1,66 @@
 import java.util.Date;
+
+import javax.swing.text.View;
+
 import java.util.ArrayList;
 
 public class VerificacioVehicles {
   public static void main(String[] args) {
     // Creem un array amb els vehicles a verificar
-    // L'algorisme ha de tenir un Array[] d'entrada amb tots els vehicles que s'han de verificar. Aquests vehicles tindran una matrícula, una data de matriculació i tipus de carburant GASOLINA | GASOIL.
-    String [][]  Vehicle = 
-    Vehicle[] vehicles = {
-      new Vehicle("1234ABC", new Date(2020, 1, 1), "GASOLINA"),
-      new Vehicle("5678DEF", new Date(2019, 5, 15), "DIESEL"),
-      new Vehicle("9101GHI", new Date(2018, 8, 30), "GASOLINA"),
-      new Vehicle("2345JKL", new Date(2009, 11, 1), "DIESEL"),
-      new Vehicle("6789MNO", new Date(2007, 2, 15), "GASOLINA"),
-      new Vehicle("9012PQR", new Date(2004, 5, 30), "DIESEL")
+    // L'algorisme ha de tenir un Array[] d'entrada amb tots els vehicles que s'han
+    // de verificar.
+    // Aquests vehicles tindran una matrícula, una data de matriculació i tipus de
+    // carburant GASOLINA | GASOIL.
+
+    // Array de vehicles
+    Object[][] vehicles = {
+        // , Name, Phone
+        { "1234ABC", "2020-01-01", "GASOLINA" },
+        { "1234ABC", "2002-5-15", "DIESEL" },
+        { "1234ABC", "2018-8-30", "DIESEL" },
+        { "1234ABC", "2009-11-01", "GASOLINA" },
+        { "1234ABC", "2007-2-15", "GASOLINA" },
+        { "1234ABC", "2004-5-30", "DIESEL" }
+
     };
 
+    int cont2010= 0;
     // Creem una llista per emmagatzemar els vehicles retirats
-    ArrayList<Vehicle> vehiclesRetirats = new ArrayList<>();
-    int count = 0;
-    // Recorrem l'array de vehicles
+    int retirats = 1;
+    Object[][] vehiclesRetirats = new Object[retirats][3];
+    
+    for (int i = 0; i < vehicles.length; i++) {
+      
+      // Obtenim la data de matriculació del vehicle
+      String dataMatriculacio = (String) vehicles[i][1];
+      // Obtenim els primers 4 caràcters de la data de matriculació
+      String anyMatriculacio = dataMatriculacio.substring(0, 4);
+      // Convertim els primers 4 caràcters de la data de matriculació a un enter
+      int anyMatriculacioInt = Integer.parseInt(anyMatriculacio);
+      // Comprovem anterior del 2010
+      if (anyMatriculacioInt < 2010) {
+        cont2010=cont2010+1;
+        // Comprovem anterior i si es de diesel
+        if (vehicles[i][2] == "DIESEL"){
+          // Si es del 2010 o anterior i es de diesel, el guardem a la llista de
+          // vehicles retirats
+          for (int j = 0; j < vehiclesRetirats.length; j++) {
+            retirats++;
+            vehiclesRetirats[j][0] = vehicles[i][0];
+            vehiclesRetirats[j][1] = vehicles[i][1];
+            vehiclesRetirats[j][2] = vehicles[i][2];
+            System.out.println(
+            "Vehicle retirat: " + vehiclesRetirats[j][0] + " " + vehiclesRetirats[j][1] + " " + vehiclesRetirats[j][2]);
+          }
+        }
 
-    for (Vehicle v : vehicles) {
-      // Verifiquem si el vehicle va ser matriculat abans de 2010
-      if (v.getDataMatriculacio().before(new Date(2010, 0, 1)){
-        // Contador qeu conta si el vehicle es matriculat avans del 2010
-        count++;
-      }
-      // Verifiquem si el vehicle va ser matriculat abans de 2010 i funciona amb diesel
-      if (v.getDataMatriculacio().before(new Date(2010, 0, 1)) && v.getTipusCarburant().equals("DIESEL")) {
-        // Si és així, afegim el vehicle a la llista de vehicles retirats
-        vehiclesRetirats.add(v);
+        // Si es del 2010 o anterior i es de gasolina, el guardem a la llista de
+        // vehicles retirats
+
       }
     }
 
-    // Mostrem la llista de vehicles retirats
-    System.out.println("Vehicles retirats:");
-    for (Vehicle v : vehiclesRetirats) {
-      System.out.println("- Matrícula: " + v.getMatricula() + ", Data de matriculació: " + v.getDataMatriculacio() + ", Tipus de carburant: " + v.getTipusCarburant());
-    }
+    // mostrem els vehicles retirats
+
   }
 }
-
-class Vehicle {
-  private String matricula;
-  private Date dataMatriculacio;
-  private String tipusCarburant;
-
-  public Vehicle(String matricula, Date dataMatriculacio, String tipusCarburant) {
-    this.matricula = matricula;
-    this.dataMatriculacio = dataMatriculacio;
-    this.tipusCarburant = tipusCarburant;
-  }
-
-  public String getMatricula() {
-    return matricula;
-  }
-
-  public Date getDataMatriculacio() {
-    return dataMatriculacio;
-  }
-
-  public String getTipusCarburant() {
-    return tipusCarburant;
-  }
-}
-
